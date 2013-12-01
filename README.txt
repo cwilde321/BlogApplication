d7a8dcfa0b4f6d7af6def927194c3eb4f00491ef
cwilde321 <c_wilde123@hotmail.com>
2013-12-01 16:18:40 -0500

    No changes made

---
+++ README.txt
@@ -0,0 +1,5 @@
+Blog Application
+
+
+
+This is an application that creates and deletes comments and posts. People are able to create posts to be commented on from other people. It also supports multiple tags.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'e1002480ea8bd11f43851b4a492367c7f0fe8487588edfee53048ab9b11200c5142f867d595fb68a48060502872fd9e97733ab779b642c7920b17a2dd4a040a1'
